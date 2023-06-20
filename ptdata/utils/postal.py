721f4a44662ae9b5af55cdaf6ed57f1c8f6e01f5
Tiago Simões <tiago.simoes@uni-foundation.eu>
2023-06-20 17:34:29 +0200

    Restructures postal code utilities

--- ptdata/utils/postal.py
+++ ptdata/utils/postal.py
@@ -6,37 +6,43 @@ from ptdata.utils import fancyprint as fp
 
 
 def normalize(df, src_col):
-    df[settings.CLEAN_POSTAL] = None
+    df[settings.CLEAN_CP7] = None
+    df[settings.CLEAN_CP4] = None
 
     for i, row in df.iterrows():
         postal_code = row[src_col]
-        clean_postal_code = clean(postal_code)
-        df.loc[i, settings.CLEAN_POSTAL] = clean_postal_code
+        clean_cp7, clean_cp4 = clean(postal_code)
+        df.loc[i, settings.CLEAN_CP7] = clean_cp7
+        df.loc[i, settings.CLEAN_CP4] = clean_cp4
 
 
 def clean(value):
     parts = value.split(' ')
-    match = None
+    cp7 = None
     cp4 = None
     cp3 = None
 
     for i, p in enumerate(parts):
         if re.match(r'\d{4}-\d{3}', p):
-            match = p
+            cp7 = p
+            cp4 = p[0:4]
+            cp3 = p[5:8]
             break
         elif re.match(r'\d{4}', p):
             cp4 = p
         elif re.match(r'\d{3}', p):
             cp3 = p
 
-    if not match and cp4:
+    if not cp7 and cp4:
         if cp3 and re.match(r'\d{4}-\d{3}', f'{cp4}-{cp3}'):
-            match = f'{cp4}-{cp3}'
+            cp7 = f'{cp4}-{cp3}'
         else:
             fp.warning(f'Storing partial postal code {fp.fgy(cp4)}')
-            match = cp4
 
-    if not match:
+    if not cp4:
         fp.error(f'Cannot find a match for {fp.fgr(value)}')
 
-    return match if match else None
+    cp7 = cp7 if cp7 else None
+    cp4 = cp4 if cp4 else None
+
+    return cp7, cp4
