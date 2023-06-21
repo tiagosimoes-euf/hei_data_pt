1154a5f9205162c33cc558107e32ab6562cb0e1e
Tiago Simões <tiago.simoes@uni-foundation.eu>
2023-06-21 18:48:25 +0200

    Restructures match protocol, includes manual input

--- ptdata/scripts/match.py
+++ ptdata/scripts/match.py
@@ -1,19 +1,14 @@
 
 from ptdata import settings
-from ptdata.utils import db, fancyprint as fp, postal, url
+from ptdata.utils import db, fancyprint as fp, interact, postal, url
 
 
 def main(*args):
-    human_readable = 'Matching DGEEC and ECHE data'
-    fp.start(human_readable)
-
     df_dgeec = load_dgeec()
     df_eche = load_eche()
 
     match_clean(df_dgeec, df_eche)
     # db.save(df_match, 'match')
-
-    fp.end(human_readable)
 
 
 def load_dgeec():
@@ -64,6 +59,9 @@ def load_eche():
 
 
 def match_clean(df_dgeec, df_eche):
+    human_readable = 'Matching DGEEC and ECHE data'
+    fp.start(human_readable)
+
     dgeec_cols = [
         'nomeEstabelecimento',
         settings.DGEEC_ID,
@@ -81,6 +79,7 @@ def match_clean(df_dgeec, df_eche):
     ]
 
     match_map = {}
+    manual_map = {}
 
     eche_fqdn = df_eche[settings.CLEAN_FQDN].to_list()
     eche_fqdn_dups = [f for f in eche_fqdn if eche_fqdn.count(f) > 1]
@@ -92,27 +91,34 @@ def match_clean(df_dgeec, df_eche):
         eche_fqdn = row[settings.CLEAN_FQDN]
         eche_cp7 = row[settings.CLEAN_CP7]
 
-        if eche_fqdn and eche_fqdn in eche_fqdn_dups:
-            fp.error(f'Cannot match because {fp.fgr(eche_fqdn)} is duplicated')
-            print(df_eche[df_eche[settings.CLEAN_FQDN] == eche_fqdn])
-        else:
-            df_match_fqdn = df_dgeec[df_dgeec[settings.CLEAN_FQDN] == eche_fqdn]
+        df_match_fqdn = df_dgeec[df_dgeec[settings.CLEAN_FQDN] == eche_fqdn]
 
-            if len(df_match_fqdn) > 1:
-                df_match_fqdn = df_match_fqdn[df_match_fqdn['depende'].isnull()].copy()
+        if len(df_match_fqdn) > 1:
+            df_match_fqdn = df_match_fqdn[df_match_fqdn['depende'].isnull()].copy()
 
-            if not df_match_fqdn.empty:
-                if len(df_match_fqdn) == 1:
-                    match_fqdn = df_match_fqdn.squeeze()
-                    dgeec_name = match_fqdn['nomeEstabelecimento']
-                    fp.success(f'{fp.fgg(eche_name)} matches {fp.fgg(dgeec_name)}')
-                    match_map[eche_code] = match_fqdn[settings.DGEEC_ID]
-                else:
-                    fp.warning(f'Multiple matches for {fp.fgy(eche_fqdn)}')
-                    print(df_eche.loc[[i]][eche_cols])
-                    print(df_match_fqdn[dgeec_cols])
+        if not df_match_fqdn.empty:
+            if len(df_match_fqdn) == 1:
+                match_fqdn = df_match_fqdn.squeeze()
+                dgeec_name = match_fqdn['nomeEstabelecimento']
+                # fp.success(f'{fp.fgg(eche_name)} matches {fp.fgg(dgeec_name)}')
+                match_map[eche_code] = match_fqdn[settings.DGEEC_ID]
+            else:
+                fp.warning(f'Multiple matches for FQDN {fp.fgy(eche_fqdn)}')
+                print(df_eche.loc[[i]][eche_cols])
+                print(df_match_fqdn[dgeec_cols])
 
-        if eche_code not in match_map:
+                options = df_match_fqdn[settings.DGEEC_ID].to_list()
+                selected = interact.choose_from(options)
+
+                if selected:
+                    df_match_sel = df_match_fqdn[df_match_fqdn[settings.DGEEC_ID] == selected]
+                    match_sel = df_match_sel.squeeze()
+                    dgeec_name = match_sel['nomeEstabelecimento']
+
+                    fp.success(f'{fp.fgg(eche_name)} mapped to {fp.fgg(dgeec_name)}')
+                    manual_map[eche_code] = selected
+
+        if eche_code not in [*match_map.keys(), *manual_map.keys()]:
             df_match_cp7 = df_dgeec[df_dgeec[settings.CLEAN_CP7] == eche_cp7]
 
             if len(df_match_cp7) > 1:
@@ -122,15 +128,42 @@ def match_clean(df_dgeec, df_eche):
                 if len(df_match_cp7) == 1:
                     match_cp7 = df_match_cp7.squeeze()
                     dgeec_name = match_cp7['nomeEstabelecimento']
-                    fp.success(f'{fp.fgg(eche_name)} matches {fp.fgg(dgeec_name)}')
+                    # fp.success(f'{fp.fgg(eche_name)} matches {fp.fgg(dgeec_name)}')
                     match_map[eche_code] = match_cp7[settings.DGEEC_ID]
                 else:
-                    fp.warning(f'Multiple matches for {fp.fgy(eche_cp7)}')
+                    fp.warning(f'Multiple matches for CP7 {fp.fgy(eche_cp7)}')
                     print(df_eche.loc[[i]][eche_cols])
                     print(df_match_cp7[dgeec_cols])
 
-        if eche_code not in match_map:
-            fp.error(f'No matches for {fp.fgr(eche_code)}')
+                    options = df_match_cp7[settings.DGEEC_ID].to_list()
+                    selected = interact.choose_from(options)
+
+                    if selected:
+                        df_match_sel = df_match_cp7[df_match_cp7[settings.DGEEC_ID] == selected]
+                        match_sel = df_match_sel.squeeze()
+                        dgeec_name = match_sel['nomeEstabelecimento']
+
+                        fp.success(f'{fp.fgg(eche_name)} mapped to {fp.fgg(dgeec_name)}')
+                        manual_map[eche_code] = selected
+
+        if eche_code not in [*match_map.keys(), *manual_map.keys()]:
+            fp.warning(f'Cannot match any data for {fp.fgy(eche_code)}')
             print(df_eche.loc[[i]][eche_cols])
 
-    fp.notice(f'Matched {fp.fgc(len(match_map))} out of {fp.fgc(len(df_eche))}')
+            raw_value = interact.provide_value()
+            value = int(raw_value) if raw_value.isdigit() else None
+            if value and value in df_dgeec[settings.DGEEC_ID].to_list():
+                df_match_value = df_dgeec[df_dgeec[settings.DGEEC_ID] == value]
+                match_value = df_match_value.squeeze()
+                dgeec_name = match_value['nomeEstabelecimento']
+
+                fp.success(f'{fp.fgg(eche_name)} mapped to {fp.fgg(dgeec_name)}')
+                manual_map[eche_code] = value
+            else:
+                fp.error(f'No matches for {fp.fgr(eche_code)}')
+
+    total_matches = len(match_map) + len(manual_map)
+    fp.notice(f'Matched {fp.fgc(total_matches)} out of {fp.fgc(len(df_eche))}')
+    print(manual_map)
+
+    fp.end(human_readable)
